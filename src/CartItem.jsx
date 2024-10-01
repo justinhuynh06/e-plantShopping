894383a939b8994d5e62f210f88ae768c725c50d
Justin Huynh <justinhuynh0506@gmail.com>
2024-10-01 02:44:47 -0700

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,48 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach((item) => {
+      totalCost += item.cost * item.quantity;
+    });
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
+  };
+  
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
-
-
   const handleIncrement = (item) => {
+    const plantItem = {
+      name: item.name,
+      quantity: item.quantity + 1,
+    }
+    dispatch(updateQuantity(plantItem));
   };
 
   const handleDecrement = (item) => {
-   
+    const plantItem = {
+      name: item.name,
+      quantity: item.quantity - 1,
+    }
+    if(plantItem.quantity == 0) {
+      dispatch(removeItem(item.name));
+    } else {
+      dispatch(updateQuantity(plantItem));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+     return item.cost * item.quantity;
   };
 
   return (
